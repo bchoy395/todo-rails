a96e442eec1e7f3e482e4a52b98a9f9fa59e4088
briannaachoy <bchoy@iqtalentpartners.com>
2023-11-10 14:04:13 -0800

    start a project using https://www.colby.so/posts/turbo-rails-101-todo-list

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2023_11_06_190109) do
   create_table "todos", force: :cascade do |t|
     t.string "name"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
